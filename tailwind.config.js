db29880d0ac69adda12433888be5ad3ead1bb4f0
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-12 00:27:24 -0400

    rearranged file structure

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-  content: ['./src/**/*.{html,js}'],
+  content: ['./*.{html,js}'],
   theme: {
     fontFamily: {
       'chivo-light': ['Chivo', 'sans-serif'],
     },
     extend: {
       backgroundImage: {
-        'mobile-pattern': "url('/src/assets/mobile/image-host.jpg')",
-        'tablet-pattern': "url('/src/assets/tablet/image-host.jpg')",
-        'desktop-pattern':
-          "url('/src/assets/desktop/image-host.jpg')",
+        'mobile-pattern': "url('/assets/mobile/image-host.jpg')",
+        'tablet-pattern': "url('/assets/tablet/image-host.jpg')",
+        'desktop-pattern': "url('/assets/desktop/image-host.jpg')",
         'dot-pattern': "url('/assets/desktop/bg-pattern-dots.svg')",
       },
       colors: {
