7cec1dd5b435bb13791a0b96ae56359874d28377
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-11 23:48:53 -0400

    changed assets dir in index.html

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,9 +6,10 @@ module.exports = {
     },
     extend: {
       backgroundImage: {
-        'mobile-pattern': "url('/assets/mobile/image-host.jpg')",
-        'tablet-pattern': "url('/assets/tablet/image-host.jpg')",
-        'desktop-pattern': "url('/assets/desktop/image-host.jpg')",
+        'mobile-pattern': "url('/src/assets/mobile/image-host.jpg')",
+        'tablet-pattern': "url('/src/assets/tablet/image-host.jpg')",
+        'desktop-pattern':
+          "url('/src/assets/desktop/image-host.jpg')",
         'dot-pattern': "url('/assets/desktop/bg-pattern-dots.svg')",
       },
       colors: {
