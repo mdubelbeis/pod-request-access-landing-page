be4f5ac6e26edf4e525cf54ddbd87659eafeebd7
mdubelbeis <mason.dubelbeis@gmail.com>
2022-04-11 23:08:21 -0400

    added error message

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,6 @@
 module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
-    colors: {
-      'mint-green': '#54E6AF',
-      'dark-blue-grey': '#2C344B',
-      'light-blue-grey': '#5A668A',
-      'grey-smoke': '#C3CBE5',
-      'dark-blue': '#121725',
-      'pure-white': '#FFFFFF',
-    },
     fontFamily: {
       'chivo-light': ['Chivo', 'sans-serif'],
     },
@@ -18,6 +10,14 @@ module.exports = {
         'tablet-pattern': "url('/assets/tablet/image-host.jpg')",
         'desktop-pattern': "url('/assets/desktop/image-host.jpg')",
         'dot-pattern': "url('/assets/desktop/bg-pattern-dots.svg')",
+      },
+      colors: {
+        'mint-green': '#54E6AF',
+        'dark-blue-grey': '#2C344B',
+        'light-blue-grey': '#5A668A',
+        'grey-smoke': '#C3CBE5',
+        'dark-blue': '#121725',
+        'pure-white': '#FFFFFF',
       },
     },
   },
